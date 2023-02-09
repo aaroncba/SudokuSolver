c3445af0ab3d80d27cf9a643ffb877ba04e6efff
Aaron Isaac Colindres Barralaga <aaron.co.ba03@gmail.com>
2023-02-09 10:56:03 -0600

    Adding TODO

--- NewSudokuSolver.java
+++ NewSudokuSolver.java
@@ -1,4 +1,10 @@
 public class NewSudokuSolver {
+    /*
+    * TODO
+    *   -Analize the old code
+    *   -Create a new code and document each function
+    * */
+
     public static void main(String[] args) {
         System.out.println("This shit deleted my code");
     }
